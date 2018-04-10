cabfd2e6fbf7f3a8f7fdf4f185bdba161fda2e09
balatzar <bataprod@gmail.com>
2018-04-10 23:46:54 +0200

    Add pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180410193825) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "haikouilles", force: :cascade do |t|
     t.string "first"
     t.string "second"
